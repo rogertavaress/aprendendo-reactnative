8a1706436f42ba794a6fdf31ac4641e6edf3232f
Rogério Tavares <rogerioctf@gmail.com>
2020-03-12 11:52:50 -0300

    React Navigation e Configurando StatusBar

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  text2: {
-    fontSize: 18,
-    color: '#aaa',
-  },
-});
-
-console.tron.log('Olá Mundo!!');
+import Routes from './routes';
 
 function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Olá Mundo!</Text>
-      <Text style={styles.text2}>Meu primeiro App react-native.</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
   );
 }
 
